d6e3d7fd51fec7e341a355033727c15882c67642
Mostafa Habib <habib.mostafa@gmail.com>
2018-04-08 20:42:13 -0400

    Remove command line printing.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,11 +1,5 @@
 package hello;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -16,23 +10,4 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
-    }
-
-    /*
-    Run mvn package && java -jar target/properties-search-edit-form-0.1.0.jar in properties-search-edit-form directory
-    and see the beans provided by Spring Boot printed out.
-     */
 }
